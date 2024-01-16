6e319b78ca4f3e908ba85c7261dca9f223faac00
Vladilav Berezovskiy <qedirast@gmail.com>
2024-01-16 11:19:22 +0700

    feat: added best-time-to-buy-and-sell-stock

--- problems/best-time-to-buy-and-sell-stock/main.go
+++ problems/best-time-to-buy-and-sell-stock/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	r := maxProfit([]int{7, 1, 5, 3, 6, 4})
+	//						1     1        1      1    3
+	// найти все
+
+	fmt.Println(r)
+}
+
+func maxProfit(prices []int) int {
+	maxProfit, minPrice := 0, prices[0]
+
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		} else if (prices[i] - minPrice) > maxProfit {
+			maxProfit = prices[i] - minPrice
+		}
+	}
+
+	return maxProfit
 }
